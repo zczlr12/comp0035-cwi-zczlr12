d8ed9abf8e55e31753bfa2b90177b23e7448e26a
Renkai Liu <renkai.liu.20@ucl.ac.uk>
2023-11-05 00:10:18 +0000

    Completed the code for data preparation

--- src/coursework1/coursework1.md
+++ src/coursework1/coursework1.md
@@ -1,18 +1,19 @@
-# COMP0035 2023/24 Coursework 1
-## 1. Data preparation and understanding
-### 1.4 Explanation of code preparation & understanding
-## 2. Product and project definition
-### 2.1 Problem statement
-### 2.2 Product overview
+Author of the raw dataset: Paolo Mancuso, Veronica Piccialli, Antonio M. Sudoso (University of Rome Tor Vergata)
+# 1. Data preparation and understanding
+## 1.4 Explanation of code preparation & understanding
+### Preparing the data
+# 2. Product and project definition
+## 2.1 Problem statement
+## 2.2 Product overview
 A web app that deploys a machine learning model on a dataset representing the sales of 4 national pasta brands via a REST API to predict future pasta sales.
-### 2.3 Persona
+## 2.3 Persona
 ![Persona](persona.png)
-### 2.4 Project goal & objectives / Questions
-## 3. Tools & techniques
-### 3.1 Source code control
-### 3.2 Linting
-### 3.3 Project planning and tracking
-### 3.4 Use of AI
-## 4. Methodology
-### 4.1 Methodology selection
-## 5. References
+## 2.4 Project goal & objectives / Questions
+# 3. Tools & techniques
+## 3.1 Source code control
+## 3.2 Linting
+## 3.3 Project planning and tracking
+## 3.4 Use of AI
+# 4. Methodology
+## 4.1 Methodology selection
+# 5. References
--- src/coursework1/data_prep.py
+++ src/coursework1/data_prep.py
@@ -1,1 +1,76 @@
 # Data preparation and understanding code
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def rename_column(column_name):
+    """
+    Renames a column name to a more readable format
+
+    Args:
+        column_name: Name of the column to be renamed
+
+    Returns:
+        Renamed column name
+    """
+    new_column = column_name.replace("QTY_B", "Quantity sold for brand ")
+    new_column = new_column.replace("PROMO_B", "Promotion flag for brand ")
+    new_column = new_column.replace("_", " item ")
+    return new_column.strip()
+
+
+def prepare_data(df):
+    """
+    Prepares the data for analysis
+
+    Args:
+        df: Pandas DataFrame from the dataset
+
+    """
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    df_prepared = df.rename(columns=rename_column)
+    prepared_data_file = Path(__file__).parent.joinpath('dataset_prepared.csv')
+    df_prepared.to_csv(prepared_data_file, index=False)
+    return df_prepared
+
+
+def print_df_information(df):
+    """
+    Prints general information about the DataFrame
+
+    Args:
+        df: Pandas DataFrame from the dataset
+    """
+
+    print("\nNumber of rows:\n")
+    print(df.shape[0])
+    print("\nNumber of columns:\n")
+    print(df.shape[1])
+
+    print("\nFirst 5 rows:\n")
+    print(df.head())
+
+    print("\nColumn labels, datatypes and value counts:\n")
+    print(df.info(verbose=True))
+
+    print("\nGeneral statistics:\n")
+    print(df.describe())
+
+    print("\nNumber of missing values per column:\n")
+    print(df.isnull().sum())
+
+    print("\nRows with missing values:\n")
+    print(df[df.isnull().any(axis=1)])
+
+    print("\nNumber of duplicates:\n")
+    print(df.duplicated().sum())
+
+
+if __name__ == '__main__':
+    raw_data_file = Path(__file__).parent.joinpath('dataset.csv')
+    raw_df = pd.read_csv(raw_data_file)
+
+    print_df_information(raw_df)
+    prepared_df = prepare_data(raw_df)
+    print_df_information(prepared_df)
